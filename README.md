e8e65fd1df9ede038fd5bb8f75e54ba6137a54e5
freewind <nowindlee@gmail.com>
2018-04-17 23:56:33 +0800

    curve25519 demo

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-Maven Demo
-==========
+Curve25519 Demo
+===============
 
-A very simple maven project demo.
-
-```
-mvn clean install
-```
+Used in some blockchain project, e.g. <https://github.com/Bytom/bytom>, but this is a Java version.
 
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,22 @@
 package demo;
 
+import org.whispersystems.curve25519.Curve25519;
+import org.whispersystems.curve25519.Curve25519KeyPair;
+
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Curve25519 cipher = Curve25519.getInstance(Curve25519.BEST);
+        Curve25519KeyPair keyPair = cipher.generateKeyPair();
+
+        byte[] privateKey = keyPair.getPrivateKey();
+        byte[] publicKey = keyPair.getPublicKey();
+        byte[] sharedSecret = cipher.calculateAgreement(publicKey, privateKey);
+
+        byte[] signature = cipher.calculateSignature(privateKey, "Hello, world!".getBytes());
+
+        boolean validSignature = cipher.verifySignature(publicKey, "Hello, world!".getBytes(), signature);
+        System.out.println("validSignature: " + validSignature);
     }
 
 }
